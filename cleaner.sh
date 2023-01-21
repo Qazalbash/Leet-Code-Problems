72208225b98d583b96402a8e7af2b35d1b33e399
MAQ <mesumali26.ma@gmail.com>
2023-01-21 19:03:42 +0500

    shell script updated

--- cleaner.sh
+++ cleaner.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-for f in *;
-do
-    if [ -d "$f/Accepted" ];
-	then
-        cd "$f/Accepted"
+for f in *; do
+	if [ -d "$f/Accepted" ]; then
+		cd "$f/Accepted"
 		latest=$(ls -t | head -1)
 		cp "$latest/Solution.py" ../../"$f.py"
 		cp "$latest/Solution.cpp" ../../"$f.cpp"
 		cd ../..
-    fi
-	if [ "$f" == "cleaner.sh" ] || [ "$f" == "README.md" ];
-	then
+	fi
+	if [ "$f" == ".gitignore" ] || [ "$f" == "cleaner.sh" ] || [ "$f" == "README.md" ]; then
 		continue
 	fi
+	rm -rf "$f"
 done
